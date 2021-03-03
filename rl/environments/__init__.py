02fdfdfdd4c4c942873009ca6a6d3d74379baa30
Jakob Stigenberg <jakob@jakke.se>
2021-03-03 13:02:55 +0100

    added base env

--- rl/environments/__init__.py
+++ rl/environments/__init__.py
@@ -1,2 +1,5 @@
 """Module containing the abstract definition of an environment, as well as
 implementations of it."""
+
+from .environment import Environment
+from .simulator_wrapper import SimulatorWrapper
