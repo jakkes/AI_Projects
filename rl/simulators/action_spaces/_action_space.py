a4b8c095331dec19c60d0752503e3244506fd371
Jakob Stigenberg <jakob@jakke.se>
2021-03-03 20:07:13 +0100

    started on action space in simulator instead

---
+++ rl/simulators/action_spaces/_action_space.py
@@ -0,0 +1,55 @@
+from abc import ABC, abstractclassmethod
+from typing import Any, TypeVar
+
+import rl.simulators.action_spaces as action_spaces
+
+T = TypeVar("T")
+
+
+class ActionSpace(ABC):
+
+    @classmethod
+    def as_type(cls, t: T) -> T:
+        """Casts the action space to the specific type.
+
+        Args:
+            t (T): Type to which the space should be cast
+
+        Raises:
+            RuntimeError: If the object is not castable to the requested type.
+
+        Returns:
+            T: Casted version of the class.
+        """
+        if not isinstance(cls, t):
+            raise RuntimeError(f"Failed casting {cls} to {t}")
+        return cls
+
+    def as_discrete(self) -> action_spaces.DiscreteActionSpace:
+        """Casts this object to a discrete action space. This operation is equivalent
+        to `as_type(DiscreteActionSpace)`."""
+        return self.as_type(action_spaces.DiscreteActionSpace)
+
+    @abstractclassmethod
+    def sample(self) -> Any:
+        """Samples an action from the action space.
+
+        Returns:
+            Any: An action.
+        """
+        raise NotImplementedError
+
+    @abstractclassmethod
+    def contains(self, action: Any) -> bool:
+        """Determines if the given action is in the action space or not.
+
+        Args:
+            action (Any): Action.
+
+        Returns:
+            bool: True if the given action is legal, otherwise False.
+        """
+        raise NotImplementedError
+
+    def __contains__(self, action: Any) -> bool:
+        return self.contains(action)
---
+++ rl/simulators/action_spaces/_discrete_action_space.py
@@ -0,0 +1,32 @@
+from abc import abstractmethod
+
+import numpy as np
+
+from ._action_space import ActionSpace
+
+
+class DiscreteActionSpace(ActionSpace):
+
+    def __init__(self, size: int) -> None:
+        super().__init__()
+        self._size = size
+
+    @property
+    def size(self):
+        """The size of the discrete action space."""
+        return self._size
+
+    @property
+    @abstractmethod
+    def action_mask(self) -> np.ndarray:
+        """The boolean action mask of the current environmental state. Legal actions
+        are marked with `True` and illegal actions with `False`."""
+        raise NotImplementedError
+
+    def sample(self) -> int:
+        return np.random.choice(np.arange(self._size)[self.action_mask])
+
+    def contains(self, action: int) -> bool:
+        if not isinstance(action, (int, np.integer)):
+            return False
+        return self.action_mask[action]
