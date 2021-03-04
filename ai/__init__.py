eab0ee7d0bfa4ac50260f8fd5b7a89502d422702
Jakob Stigenberg <jakob@jakke.se>
2021-03-04 15:55:27 +0100

    bump version

--- ai/__init__.py
+++ ai/__init__.py
@@ -2,7 +2,7 @@ from multiprocessing import set_start_method, get_start_method
 from . import environments, simulators, utils, agents
 
 __all__ = ["environments", "simulators", "utils", "agents"]
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 try:
     set_start_method("spawn")
