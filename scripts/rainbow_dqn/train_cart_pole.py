6b949321cff73c42429bea7e6355a67ddeeec68f
Jakob Stigenberg <jakob@jakke.se>
2021-04-09 15:10:12 +0200

    add cmd args

--- scripts/rainbow_dqn/train_cart_pole.py
+++ scripts/rainbow_dqn/train_cart_pole.py
@@ -1,5 +1,5 @@
 import tap
-from torch import nn, optim
+from torch import optim
 
 import ai.environments as environments
 import ai.agents.dqn.rainbow as rainbow
@@ -9,25 +9,17 @@ class ArgumentParser(tap.Tap):
     episodes: int = 100
     """Number of episodes to run the training for."""
 
+    batch_size: int = 32
+    """Batch size used by the agent."""
 
-class Network(nn.Module):
-    def __init__(self, distributional: bool, atoms: int):
-        super().__init__()
-        self.a = nn.Linear(4, 32)
-        self.b = nn.ReLU(inplace=True)
-        self.c = nn.Linear(32, 2 * atoms if distributional else 2)
+    target_update_steps: int = 10
+    """Number of update steps between target network updates."""
 
-        self.dist = distributional
-        self.atoms = atoms
+    discount_factor: float = 0.99
+    """Discount factor."""
 
-    def forward(self, x):
-        x = self.a(x)
-        x = self.b(x)
-        x = self.c(x)
-
-        if self.dist:
-            x = x.view(-1, 2, self.atoms)
-        return x
+    replay_capacity: int = 8192
+    """Replay capacity."""
 
 
 def main(args: ArgumentParser):
@@ -35,17 +27,22 @@ def main(args: ArgumentParser):
 
     agent_config = rainbow.AgentConfig()
     agent_config.action_space_size = 2
-    agent_config.state_shape = (4, )
-    agent_config.batch_size = 32
-    agent_config.target_update_steps = 10
-    agent_config.discount_factor = 0.99
-    agent_config.replay_capacity = 8192
+    agent_config.state_shape = (4,)
+    agent_config.batch_size = args.batch_size
+    agent_config.target_update_steps = args.target_update_steps
+    agent_config.discount_factor = args.discount_factor
+    agent_config.replay_capacity = args.replay_capacity
 
-    agent_config.use_prioritized_experience_replay = True
+    agent_config.use_prioritized_experience_replay = False
     agent_config.use_distributional = True
     agent_config.use_double = True
+    agent_config.n_atoms = 21
+    agent_config.v_min = 0
+    agent_config.v_max = 100
 
-    network = Network(agent_config.use_distributional, agent_config.n_atoms)
+    network = rainbow.networks.CartPole(
+        agent_config.use_distributional, agent_config.n_atoms, 0.5
+    )
     optimizer = optim.Adam(network.parameters(), lr=1e-4)
     agent = rainbow.Agent(agent_config, network, optimizer)
 
