073d3abd52967dcbf59238d2c39b1155621e1eb2
Jakob Stigenberg <jakob@jakke.se>
2021-04-09 09:21:15 +0200

    Add gym wrapper (#5)
    * add gym as requirement
    
    * add action space wrapper
    
    * add env wrapper
    
    * add test
    
    * fix test
    
    Co-authored-by: Jakob Stigenberg <jakob.stigenberg@king.com>

---
+++ ai/environments/_gym_wrapper.py
@@ -0,0 +1,36 @@
+from typing import Tuple, Dict
+
+import numpy as np
+import gym
+
+import ai.environments as environments
+
+
+class Factory(environments.Factory):
+    def __init__(self, env_id: str):
+        self.env_id = env_id
+
+    def __call__(self) -> "GymWrapper":
+        return GymWrapper(gym.make(self.env_id))
+
+
+class GymWrapper(environments.Base):
+    def __init__(self, env: gym.Env):
+        self._env = env
+        self._action_space = environments.action_spaces.DiscreteGymWrapper(
+            env.action_space
+        )
+
+    @property
+    def action_space(self) -> environments.action_spaces.Base:
+        return self._action_space
+
+    def step(self, action: int) -> Tuple[np.ndarray, float, bool, Dict]:
+        return self._env.step(action)
+
+    def reset(self) -> np.ndarray:
+        return self._env.reset()
+
+    @classmethod
+    def get_factory(cls, env_id: str) -> Factory:
+        return Factory(env_id)
