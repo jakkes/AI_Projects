879df2d810397119ca5ef84b0f668ee3a3c213eb
Jakob Stigenberg <jakob@jakke.se>
2021-04-10 19:28:05 +0200

    fix import order

--- ai/simulators/__init__.py
+++ ai/simulators/__init__.py
@@ -2,11 +2,11 @@
 implementations of it."""
 
 
-from ._tictactoe import TicTacToe
-from ._base import Base
-from ._connect_four import ConnectFour
-from ._factory import Factory
 from . import action_spaces
+from ._base import Base
+from ._factory import Factory
+from ._connect_four import ConnectFour
+from ._tictactoe import TicTacToe
 
 
 __all__ = ["TicTacToe", "Base", "ConnectFour", "Factory", "action_spaces"]
