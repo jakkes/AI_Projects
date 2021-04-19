8976f5f872a546590e8ae3723aa60a81fc8b840b
Jakob Stigenberg <jakob.stigenberg@king.com>
2021-04-19 08:22:12 +0200

    log loss

--- ai/agents/__init__.py
+++ ai/agents/__init__.py
@@ -1,6 +1,6 @@
 """Module containing implementations of AI algorithms."""
 
-from . import alpha_zero, utils, dqn
+from . import alpha_zero, utils, dqn, a3c
 
 
-__all__ = ["alpha_zero", "utils", "dqn"]
+__all__ = ["alpha_zero", "utils", "dqn", "a3c"]
