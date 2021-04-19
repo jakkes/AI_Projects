2806925b00a1f225d1e8c1df37a3c2ca3024ef63
Jakob Stigenberg <jakob@jakke.se>
2021-04-19 09:28:37 +0200

    a3c reimplemented (#12)
    * prepping
    
    * a3c kinda done
    
    * debugging
    
    * now runs
    
    * added logging
    
    * log loss
    
    * log loss
    
    * typo
    
    * plot gradient magnitude
    
    * add agent
    
    * test agent
    
    * choice check for nans
    
    * add entropy
    
    * reduce
    
    * train with entropy
    
    * skip entropy for now
    
    Co-authored-by: Jakob Stigenberg <jakob.stigenberg@king.com>

---
+++ ai/agents/a3c/trainer/_logger.py
@@ -0,0 +1,19 @@
+import queue
+
+from torch.utils.tensorboard.writer import SummaryWriter
+import ai.utils.logging as logging
+
+
+class Logger(logging.SummaryWriterServer):
+    def __init__(self, data_queue: queue.Queue):
+        super().__init__("a3c", data_queue)
+
+    def log(self, summary_writer: SummaryWriter, data):
+        if "r" in data:
+            summary_writer.add_scalar("Episode/Reward", data["r"])
+        if "v" in data:
+            summary_writer.add_scalar("Episode/Start value", data["v"])
+        if "l" in data:
+            summary_writer.add_scalar("Agent/Loss", data["l"])
+        if "gm" in data:
+            summary_writer.add_scalar("Agent/Grad. mag.", data["gm"])
