5792050cc7691813e1aa5bd58d5e6c17930ba383
Jakob Stigenberg <jakob@jakke.se>
2021-05-01 16:47:34 +0200

    add to path

--- ai/simulators/__init__.py
+++ ai/simulators/__init__.py
@@ -8,6 +8,15 @@ from ._factory import Factory
 from ._connect_four import ConnectFour
 from ._tictactoe import TicTacToe
 from ._grid import Grid
+from ._cart_pole import CartPole
 
 
-__all__ = ["TicTacToe", "Base", "ConnectFour", "Factory", "action_spaces", "Grid"]
+__all__ = [
+    "TicTacToe",
+    "Base",
+    "ConnectFour",
+    "Factory",
+    "action_spaces",
+    "Grid",
+    "CartPole",
+]
