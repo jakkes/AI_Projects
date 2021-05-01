25a6c2b38cee2f17f8ea9246895da5f8ea96ecf8
Jakob Stigenberg <jakob@jakke.se>
2021-05-01 11:49:12 +0200

    fix docstring

--- ai/simulators/_factory.py
+++ ai/simulators/_factory.py
@@ -7,12 +7,16 @@ T = TypeVar("T")
 class Factory(Generic[T]):
     """Factories are callable objects that spawn simulator instances."""
 
+    __pdoc__ = {
+        "Factory.__call__": True
+    }
+
     def __init__(self, cls: T, *args, **kwargs):
         """
         Args:
             cls (T): Simulator class.
-            *args, **kwargs: arguments and key-word arguments passed to the simulator
-                __init__ method.
+            `*args, **kwargs`: arguments and key-word arguments passed to the simulator
+                `__init__` method.
         """
         super().__init__()
         self._cls = cls
