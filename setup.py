91ccf1784bd6d0d57bbc6517876fb5111c28c357
Jakob Stigenberg <jakob@jakke.se>
2021-09-10 20:48:44 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     install_requires=[
         "torch~=1.9.0",
         "scipy~=1.6.1",
-        "tensorboard~=2.6.0",
+        "tensorboard<3",
         "gym~=0.18.0",
         "typed-argument-parser~=1.6.2",
         "python-linq~=2.0.3"
