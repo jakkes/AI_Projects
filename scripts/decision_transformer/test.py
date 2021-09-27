e2fe1f62c9fd57b1a02b7e4fb1aaf79ca03286ff
Jakob Stigenberg <jakob@jakke.se>
2021-09-27 20:43:55 +0200

    smaller nets and use quantile exploration

--- scripts/decision_transformer/test.py
+++ scripts/decision_transformer/test.py
@@ -17,9 +17,10 @@ class Transformer(nn.Module):
         super().__init__()
         self._lstm = nn.LSTM(
             input_size=EMBED_SPACE,
-            hidden_size=EMBED_SPACE,
+            hidden_size=32,
             num_layers=4,
             batch_first=True,
+            proj_size=EMBED_SPACE
         )
 
     def forward(self, x):
@@ -30,9 +31,7 @@ class Transformer(nn.Module):
 class StateEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(4, 32), nn.ReLU(inplace=True), nn.Linear(32, EMBED_SPACE)
-        )
+        self.seq = nn.Linear(4, EMBED_SPACE)
 
     def forward(self, x):
         return self.seq(x)
@@ -41,9 +40,7 @@ class StateEncoder(nn.Module):
 class ActionEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(1, 16), nn.ReLU(inplace=True), nn.Linear(16, EMBED_SPACE)
-        )
+        self.seq = nn.Linear(1, EMBED_SPACE)
 
     def forward(self, x):
         return self.seq(x.unsqueeze(-1))
@@ -52,9 +49,7 @@ class ActionEncoder(nn.Module):
 class RewardEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(1, 16), nn.ReLU(inplace=True), nn.Linear(16, EMBED_SPACE)
-        )
+        self.seq = nn.Linear(1, EMBED_SPACE)
 
     def forward(self, x):
         return self.seq(x.unsqueeze(-1))
@@ -63,9 +58,7 @@ class RewardEncoder(nn.Module):
 class PositionEncoder(nn.Module):
     def __init__(self):
         super().__init__()
-        self.seq = nn.Sequential(
-            nn.Linear(1, 16), nn.ReLU(inplace=True), nn.Linear(16, EMBED_SPACE)
-        )
+        self.seq = nn.Linear(1, EMBED_SPACE)
 
     def forward(self, x):
         return self.seq(x.unsqueeze(-1))
@@ -86,13 +79,13 @@ def main():
     config = df.TrainerConfig(
         state_shape=(4,),
         action_size=2,
-        batch_size=1,
+        batch_size=32,
         max_episode_steps=200,
         number_of_actors=6,
         replay_capacity=10000,
         min_replay_size=5000,
         training_time=3600,
-        inference_sequence_length=5,
+        inference_sequence_length=15,
         enable_float16=True,
         enable_cuda=True,
         inference_batchsize=5
@@ -116,6 +109,7 @@ def main():
         ),
         lr=1e-4,
         eps=1e-6,
+        weight_decay=1e-4
     )
 
     trainer = df.Trainer(
@@ -127,7 +121,7 @@ def main():
         action_decoder,
         *empty_embeddings,
         environments.GymWrapper.Factory("CartPole-v0"),
-        df.exploration_strategies.MaxObserved(0.0),
+        df.exploration_strategies.Quantile(0.75),
         config,
         optimizer
     )
