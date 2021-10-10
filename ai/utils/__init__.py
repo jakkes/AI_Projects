a48ca4fb8ffcf07ff6d7d1e37ce648dc5c5ceb01
Jakob Stigenberg <jakob@jakke.se>
2021-10-10 18:16:37 +0200

    metronome

--- ai/utils/__init__.py
+++ ai/utils/__init__.py
@@ -1,5 +1,6 @@
 """Module containing general utility methods, used throughout the library."""
 
+from ._metronome import Metronome
 from . import np, torch, logging
 
-__all__ = ["np", "torch", "logging"]
+__all__ = ["np", "torch", "logging", "Metronome"]
