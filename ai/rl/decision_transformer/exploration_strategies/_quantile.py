6f612a42a436fb34c2fa00d64d4d307d0df1061b
Jakob Stigenberg <jakob@jakke.se>
2021-11-01 17:38:00 +0100

    update docstring

--- ai/rl/decision_transformer/exploration_strategies/_quantile.py
+++ ai/rl/decision_transformer/exploration_strategies/_quantile.py
@@ -6,17 +6,19 @@ from ._base import Base
 class Quantile(Base):
     """Reward-to-go strategy returning a certain quantile of last seen rewards."""
 
-    def __init__(self, quantile: float, update_rate: float=0.9, momentum: float=0.99) -> None:
+    def __init__(self, quantile: float, learning_rate: float=1.0, momentum: float=0.99) -> None:
         """
         Args:
             quantile (float): Quantile, float in (0, 1).
-            update_rate (float, optional): Float in (0, 1). Controls the smoothing of
-                the quantile regression. Higher value increases smoothing. Defaults to
-                0.99.
+            learning_rate (float, optional): Learning rate used in the gradient descent
+                algorithm. Defaults to 1.0.
+            momentum (float, optional): Momentum used in the gradient descent algorithm.
+                Defaults to 0.99.
+            
         """
         super().__init__()
         self.__quantile = quantile
-        self.__alpha = 1 - update_rate
+        self.__alpha = learning_rate
         self.__momentum_rate = momentum
         self.__momentum = 0.0
         self.__value = 0.0
